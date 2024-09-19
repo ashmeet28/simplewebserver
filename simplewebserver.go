1cf7771c594ffa0e817931a84c1b3f6cf728d861
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-09-18 20:18:02 -0400

    Small update

--- simplewebserver.go
+++ simplewebserver.go
@@ -7,5 +7,9 @@ import (
 )
 
 func main() {
-	log.Fatal(http.ListenAndServe(os.Args[1]+":8080", http.FileServer(http.Dir(os.Args[2]))))
+	if os.Args[1] == "public" {
+		log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir(os.Args[2]))))
+	} else if os.Args[1] == "private" {
+		log.Fatal(http.ListenAndServe("127.0.0.1:8080", http.FileServer(http.Dir(os.Args[2]))))
+	}
 }
